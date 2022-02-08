70b8e188db3d84f361001d695f817a00a16f8483
Matthew Faigan <matthew.faigan@gmail.com>
2022-02-07 19:21:35 -0500

    Add command execution functions

---
+++ ExecutionUtils.cpp
@@ -0,0 +1,55 @@
+#ifndef EXECUTION_UTILS
+#define EXECUTION_UTILS
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+namespace ExecutionUtils
+{
+    std::string executeCommand(const std::string& command)
+    {
+        char readBuffer[128];
+        std::string writeBuffer;
+        FILE* pipe;
+
+        #ifdef _WIN32
+            pipe = _popen(command.c_str(), "rt");
+        #else
+            pipe = popen(command.c_str(), "r");
+        #endif
+
+        if (pipe != NULL)
+        {
+            while (fgets(readBuffer, 128, pipe))
+            {
+                writeBuffer += readBuffer;
+            }
+
+            #ifdef _WIN32
+                _pclose(pipe);
+            #else
+                pclose(pipe);
+            #endif
+        }
+
+        return writeBuffer;
+    }
+
+    void executeCommandWithConsoleOutput(const std::string& command)
+    {
+        std::cout << executeCommand(command) << std::endl;
+    }
+
+    void executeCommandWithFileOutput(const std::string& command, const std::string& filePath, bool shouldOverwrite)
+    {
+        auto output = executeCommand(command);
+        std::ofstream outFile(filePath, shouldOverwrite ? std::ios_base::trunc : std::ios_base::app);
+        if (outFile.is_open())
+        {
+            outFile << output;
+        }
+    }
+}
+
+#endif
