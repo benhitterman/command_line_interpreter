d569f7ab814b96a661a053ad660aa8ae1202431b
alexsantelli <santellialex@gmail.com>
2022-02-09 20:55:47 -0500

    Removed GOTO

--- ShellHost.cpp
+++ ShellHost.cpp
@@ -5,11 +5,6 @@
 #include <filesystem>
 #include <sys/stat.h>
 using namespace std;
-using filesystem::current_path;
-using filesystem::exists;
-using filesystem::is_directory;
-using filesystem::is_regular_file;
-namespace fs = filesystem;
 
 bool isPathExist(const string &s)
 {
@@ -31,48 +26,58 @@ FoundExec:
         cout << user << "@" << host << "$ ";
         getline(cin, command);
         // getline(cin, userAnswer, ' ');
-        
+
         char firstWhitespace = command.find(" ");
         string command1 = command.substr(0, firstWhitespace);
-        
+
         char ampersand = command.rfind("&");
         bool isBackground = 0;
-        if (ampersand != string::npos) {
+        if (ampersand != string::npos)
+        {
             isBackground = 1;
         }
-        
+
         char firstArrow = command.find("->");
         char firstDoubleArrow = command.find("->>");
-        
-        if (firstDoubleArrow != string::npos) {
+
+        if (firstDoubleArrow != string::npos)
+        {
             argument = command.substr(firstWhitespace + 1, firstDoubleArrow - firstWhitespace - 2);
-            if (!isBackground) {
+            if (!isBackground)
+            {
                 fileName = command.substr(firstDoubleArrow + 4, command.length());
             }
-            else {
+            else
+            {
                 fileName = command.substr(firstDoubleArrow + 4, ampersand - firstDoubleArrow - 5);
             }
         }
-        else if (firstArrow != string::npos) {
+        else if (firstArrow != string::npos)
+        {
             argument = command.substr(firstWhitespace + 1, firstArrow - firstWhitespace - 2);
-            if (!isBackground) {
+            if (!isBackground)
+            {
                 fileName = command.substr(firstArrow + 3, command.length());
             }
-            else {
+            else
+            {
                 fileName = command.substr(firstArrow + 3, ampersand - firstArrow - 4);
             }
         }
-        else {
+        else
+        {
             fileName = "";
-            if (!isBackground) {
+            if (!isBackground)
+            {
                 argument = command.substr(firstWhitespace + 1, command.length());
             }
-            else {
+            else
+            {
                 argument = command.substr(firstWhitespace + 1, ampersand - firstWhitespace - 2);
-                cout <<"";
+                cout << "";
             }
         }
-        
+
         if (command == "echo")
         {
         }
@@ -91,43 +96,38 @@ FoundExec:
         }
         else
         {
-            for (int i = 0; i < path.size(); i++)
+            bool doneExecution = false;
+            for (int i = 0; i < path.size() && !doneExecution; i++)
             {
                 if (isPathExist(path[i]))
                 {
                     // checks if file exists in path
-                    vector<string> executable;
-                    // checking if user entered exe again
-                    if (command.substr(command.length() - 4, command.length()) != ".exe")
+                    for (const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator(path[i]))
                     {
-                        command = command.append(".exe");
-                    }
-                    // creating full directory
-                    string fullExec = "";
-                    fullExec.append(path[i]);
-                    fullExec.append(command);
-
-                    // adding .exe to end of command
-                    executable.push_back(command);
-                    for (const auto &file : executable)
-                    {
-                        if (exists(fullExec))
+                        if (entry.path().stem().string() == command)
                         {
-                            ExecutionUtils::executeCommand(fullExec);
-                            // marks execution found and leaves else statement
-                            goto FoundExec;
+                            ExecutionUtils::executeCommand(command);
+                            doneExecution = true;
+                            break;
                         }
                     }
                 }
             }
             // checks current directory
-            if (exists(command))
+            for (const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator(std::filesystem::current_path()))
             {
-                ExecutionUtils::executeCommand(command);
+                if (entry.path().stem().string() == command && !doneExecution)
+                {
+                    doneExecution = true;
+                    ExecutionUtils::executeCommand(command);
+                    break;
+                }
             }
-            else
+            if (!doneExecution)
+            {
                 // only if no exe is found through all paths and current path
                 cout << command << " is not recognized as an internal or external command, operable program or batch file." << endl;
+            }
         }
     }
 }
